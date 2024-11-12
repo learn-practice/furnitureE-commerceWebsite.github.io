9d612dd10afc3a75c2cd24c5eca8ec37a5cf5cdc
vaishnavi <deshmukhvaishnavi2207@gmail.com>
2024-11-12 20:42:29 +0530

    basic setup for E-commerce website

---
+++ Admin/server/index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+
+app.listen(8000, () => {
+  console.log("listening to 8000 port");
+});
---
+++ server/index.js
@@ -0,0 +1,6 @@
+const express= require('express');
+const app = express();
+
+app.listen(5000, () => {
+  console.log("runing on 5000 port");
+});
